26b2f6625c39d0f83278959f3ecd325e460cc526
Milorad <miloradjekic@outlook.com>
2020-03-06 15:49:29 +0100

    Updates

---
+++ precache-manifest.ae715d3f23e24182123702d696a18417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413dda6118020641f6459c8097a8caf1",
+    "url": "/majabba/index.html"
+  },
+  {
+    "revision": "435be9c1cf02f6116c27",
+    "url": "/majabba/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67437f5c712fd3acbd64",
+    "url": "/majabba/static/js/2.d1ec04ac.chunk.js"
+  },
+  {
+    "revision": "3e7eb0c51ce474724fe2295510ac9693",
+    "url": "/majabba/static/js/2.d1ec04ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435be9c1cf02f6116c27",
+    "url": "/majabba/static/js/main.0435e143.chunk.js"
+  },
+  {
+    "revision": "382124a0378e5751cf85",
+    "url": "/majabba/static/js/runtime-main.94b339a6.js"
+  }
+]);
